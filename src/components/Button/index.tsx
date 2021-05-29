910a36ba70328118574f28b72b8ad1b124fcbe21
Nathan Souza <nathanpereirasouza@gmail.com>
2021-05-29 13:39:05 -0300

    criando component Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { RectButtonProperties } from 'react-native-gesture-handler';
+
+import { Container, ButtonText } from './styles';
+
+interface ButtoProps extends RectButtonProperties {
+  children: string;
+  fontSize?: number;
+  textColor?: string;
+}
+
+const Button: React.FC<ButtoProps> = ({ children, fontSize, textColor, ...rest }) => {
+
+  const styles = {}
+
+  if (fontSize) Object.assign(styles, { fontSize })
+
+  if (textColor) Object.assign(styles, { color: textColor })
+
+  return (
+    <Container {...rest}>
+      <ButtonText style={styles}>{children}</ButtonText>
+    </Container>
+  );
+};
+
+export default Button;
