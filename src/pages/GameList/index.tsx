4e3a61aecca10fb0028d53a7da7a36a8e8737506
Nathan Souza <nathanpereirasouza@gmail.com>
2021-05-29 14:39:37 -0300

    alterando layout da lista de jogos

--- src/pages/GameList/index.tsx
+++ src/pages/GameList/index.tsx
@@ -1,10 +1,22 @@
-import AsyncStorage from '@react-native-community/async-storage';
-import { useNavigation } from '@react-navigation/core';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useNavigation } from '@react-navigation/core';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import Button from '../../components/Button';
 
-// import { Container } from './styles';
+import {
+  Author,
+  Container,
+  Title,
+  Header,
+  ButtonCreateGame,
+  GameCard,
+  GameTitle,
+  GamePlayers,
+} from './styles'
+
 interface IPlayer {
   name?: string;
   color: string;
@@ -37,12 +49,41 @@ const GameList: React.FC = () => {
   }, [])
 
   return (
-    <View>
-      <Text>{games.length} jogos encontrados</Text>
+    <Container>
+      {games.length > 0 && (
+        <Header>
+          <View style={{justifyContent: 'center'}}>
+            <Title>Conta Pontos</Title>
+            <Author>por @nathanopereira</Author>
+          </View>
+          <View>
+            <ButtonCreateGame onPress={() => navigation.navigate('CreateGame')} style={{ backgroundColor: '#06f' }}>
+              <Icon name="plus" color="#fff" size={15}/>
+            </ButtonCreateGame>
+          </View>
+        </Header>
+      )}
+
       {games.map(game => (
-        <Button onPress={() => navigation.navigate('Gameplay', { id: game.id })} key={game.id}>{game.title}</Button>
+        <GameCard onPress={() => navigation.navigate('Gameplay', { id: game.id })} key={game.id}>
+          <View>
+            <GameTitle>{game.title}</GameTitle>
+            <GamePlayers>{game.players.map((player, index) => {
+              return index === 0 ? `${player.name} (${player.score})` : ` • ${player.name} (${player.score})`
+            })}
+            </GamePlayers>
+          </View>
+        </GameCard>
       ))}
-    </View>
+
+      {games.length === 0 && (
+        <View>
+          <Title isBig>Conta Pontos</Title>
+          <Author isBig>por @nathanopereira</Author>
+          <Button onPress={() => navigation.navigate('CreateGame')} style={{ backgroundColor: '#06f' }}>Criar Jogo</Button>
+        </View>
+      )}
+    </Container>
   );
 }
 
--- src/pages/GameList/styles.ts
+++ src/pages/GameList/styles.ts
@@ -5,13 +5,20 @@ interface TitleProps {
   isBig?: boolean;
 }
 
+export const TextInterRegular = styled.Text`
+  font-family: 'Inter-Regular';
+`
+
+export const TextInterBold = styled.Text`
+  font-family: 'Inter-Bold';
+`
+
 export const Container = styled.ScrollView`
   flex: 1;
   padding: 20px 15px 20px;
 `;
 
-export const Title = styled.Text<TitleProps>`
-  font-family: 'Inter-Bold';
+export const Title = styled(TextInterBold)<TitleProps>`
   color: #000;
 
   letter-spacing: -2px;
@@ -25,18 +32,37 @@ export const Title = styled.Text<TitleProps>`
     text-align: center;
   `}
 `
-export const Author = styled.Text<TitleProps>`
-  margin-bottom: 20px;
-  font-family: 'Inter-Regular';
-
+export const Author = styled(TextInterRegular)<TitleProps>`
   ${({isBig}) => isBig && css`
+    margin-bottom: 20px;
     text-align: center;
   `}
 `
 export const Header = styled.View`
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `
 
 export const ButtonCreateGame = styled(Button)`
-
+  border-radius: 50px;
+  padding: 5px;
+  width: 50px;
+  height: 50px;
+  margin: 0;
 `
+export const GameCard = styled(Button)`
+  background-color: #ebebeb;
+  align-items: flex-start;
+  padding: 10px;
+`
+
+export const GameTitle = styled(TextInterBold)`
+  font-weight: bold;
+  font-size: 18px;
+`
+export const GamePlayers = styled(TextInterRegular)`
+  font-size: 12px;
+`
+
+
