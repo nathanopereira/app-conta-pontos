f05c2b1994ed6d2c3bf1a119a516c32ea688d305
Nathan Souza <nathanpereirasouza@gmail.com>
2021-05-29 14:01:49 -0300

    criando página principal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import { View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { useNavigation } from '@react-navigation/core';
+import React, { useCallback, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import Button from '../../components/Button';
 
-import { Container } from './styles';
+import { Container, Title, Author } from './styles';
 
-const Home: React.FC = () => {
-  return <View />;
-}
+const Home: React.FC = ({ navigation }) => {
+  // const clear = useCallback(async () => await AsyncStorage.removeItem('@contapontos'), []); useEffect(() => clear(), [])
+  return (
+    <Container>
+      <Title>Conta Pontos</Title>
+      <Author>por <Text style={{ fontWeight: 'bold', color: '#333' }}>@nathanopereira</Text></Author>
+      <Button onPress={() => navigation.navigate('CreateGame')} style={{ backgroundColor: '#06f' }}>Criar Jogo</Button>
+      <Button onPress={() => navigation.navigate('GameList')}>Ver Jogos</Button>
+    </Container>
+  );
+};
 
 export default Home;
